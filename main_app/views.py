111155907ea72855f7d6aedf4fd837fd5fcb3b62
Brendan <shaefferbrendan@gmail.com>
2024-01-09 16:24:59 -0500

    added django app skeleton with base functionality

---
+++ main_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+finches = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
+# views page functions below
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
