bce6e728002dc07bb7a939fd8f44155b0de9c8d8
Brendan <shaefferbrendan@gmail.com>
2024-01-10 15:44:14 -0500

    added routing functionality and individual detail pages for each individual entry

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,13 @@
+#all paths specific to finchcollector 
 from django.urls import path
 from . import views
 
 urlpatterns = [
    path('', views.home, name='home'),
+
    path('about/', views.about, name='about'), 
+
    path('finches/', views.finches_index, name='index'),
+
+   path('finch/<int:finch_id>/', views.finches_detail,name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
-finches = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+from .models import Finch 
+
+# finches = [
+#   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+# ]
 
 # views page functions below
 def home(request):
@@ -13,6 +15,17 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
-    return render(request, 'finches/index.html', {
-        'finches': finches
+    finch = Finch.objects.all()
+    return render(request, 'finches/index.html', 
+    {
+        'finches': finch
     })
+
+def finches_detail(request, finch_id):
+    
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', 
+    {
+        'finch': finch
+    })
+
